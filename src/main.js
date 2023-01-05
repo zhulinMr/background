2ff03731d5411b2354e224aa78783b7c9d30efa7
zhulinMr <2368890327@qq.com>
2023-01-05 17:23:43 +0800

    登录页布局

--- src/main.js
+++ src/main.js
@@ -3,8 +3,13 @@ import ElementPlus from 'element-plus'
 import 'element-plus/dist/index.css'
 import App from './App.vue'
 import router from "./router"
+import * as ElementPlusIconsVue from '@element-plus/icons-vue'
 
 const app = createApp(App)
+for (const [key, component] of Object.entries(ElementPlusIconsVue)) {
+    app.component(key, component)
+  }
+
 app.use(ElementPlus)
 import 'virtual:windi.css'
 app.use(router)
---
+++ src/pages/login.vue
@@ -0,0 +1,62 @@
+<template>
+  <el-row class="bg-indigo-500 min-h-screen">
+    <el-col :lg="16" :md="12" class="flex justify-center items-center">
+      <div style="color: #fff">欢迎光临</div>
+    </el-col>
+    <el-col
+      :lg="8"
+      :md="12"
+      class="bg-light-50 flex items-center justify-center flex-col"
+    >
+      <h2 class="font-bold text-3xl text-gray-800">欢迎回来</h2>
+      <div
+        class="my-5 text-gray-300 flex justify-center items-center space-x-2"
+      >
+        <span class="h-[1px] w-16 bg-gray-200"></span>
+        <span>账号密码登录</span>
+        <span class="h-[1px] w-16 bg-gray-200"></span>
+      </div>
+      <div>
+        <el-form :model="form" class="w-[250px]">
+          <el-form-item>
+            <el-input v-model="form.username" placeholder="请输入用户名">
+              <template #prefix>
+                <el-icon class="el-input__icon"><user /></el-icon>
+              </template>
+            </el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-input v-model="form.password" placeholder="请输入密码" >
+                 <template #prefix>
+                <el-icon class="el-input__icon"><lock /></el-icon>
+              </template>
+            </el-input>
+          </el-form-item>
+          <el-form-item>
+            <el-button
+              type="primary"
+              class="w-[250px]"
+              round
+              color="#626aef"
+              @click="onSubmit"
+              >登录</el-button
+            >
+          </el-form-item>
+        </el-form>
+      </div>
+    </el-col>
+  </el-row>
+</template>
+<script  setup>
+import { reactive } from "vue";
+// import { User,Lock } from "@element-plus/icons-vue";
+// do not use same name with ref
+const form = reactive({
+  username: "",
+  password: "",
+});
+
+const onSubmit = () => {
+  console.log("form", form);
+};
+</script>
